3b2130398d81372ddf626573e8feeb574c81da4d
weibig <wendydaan@gmail.com>
2020-04-21 20:05:35 +0800

    revise

--- seq_tag/eval.py
+++ seq_tag/eval.py
@@ -87,15 +87,14 @@ def create_seq_tag_dataset(samples, save_path, padding=0):
 
 main(args.test_data_path)
 
-
-hparams = Namespace(**{
+hparams = {
         'embedding_path': "./seq_tag/embedding.pkl",
         'embed_size': 300,
 
         'train_dataset_path': "./seq_tag/train.pkl",
         'valid_dataset_path': "./seq_tag/valid.pkl",
 
-        'test_dataset_path': args.test_data_path,####
+        'test_dataset_path': "./seq_tag/test.pkl",####
         'ignore_idx': -100,
 
         'batch_size': 16,
@@ -103,11 +102,13 @@ hparams = Namespace(**{
     #     When you increase the pos_weight, 
     #     the number of false negatives will artificially increase.
 
-        'rnn_hidden_size': 300,
-    })
+        'rnn_hidden_size': 128,
+    }
+test_hparams = Namespace(**hparams)
+seq_tagger = SeqTagger(test_hparams)
+seq_tagger.load_state_dict(torch.load("./seq_tag/seq_tag.ckpt"), strict=False)
 
-seq_tagger = SeqTagger(hparams)
-seq_tagger = seq_tagger.load_from_checkpoint("./seq_tag/seq_tag.ckpt")
+
 trainer = pl.Trainer()
 trainer.test(seq_tagger)
 
